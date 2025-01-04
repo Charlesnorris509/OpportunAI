bd7c70472f618f6aa46b5857f569c15a3e2ed31b
Charles Norris <59802926+Charlesnorris509@users.noreply.github.com>
2025-01-04 14:08:23 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,18 @@ import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:5000/api';
 
+const api = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+api.interceptors.response.use(
+    response => response,
+    error => {
+        return Promise.reject(error.response.data);
+    }
+);
+
+export default api;
 // Authentication
 export const login = async (email, password) => {
     try {
