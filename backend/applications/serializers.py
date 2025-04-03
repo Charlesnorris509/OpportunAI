5054da8c3fd1f85897e8500c2a4edbcaa04ff4fb
Charles Norris <59802926+Charlesnorris509@users.noreply.github.com>
2025-04-02 23:37:15 -0400

    Delete backend directory

--- backend/applications/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import JobApplication
-
-class JobApplicationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = JobApplication
-        fields = '__all__'
-        read_only_fields = ['user', 'application_date']
--- backend/job_tracker/wsgi.py
+++
@@ -1,6 +0,0 @@
-# 4. backend/job_tracker/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_tracker.settings')
-application = get_wsgi_application()
